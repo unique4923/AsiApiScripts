4a8deef08eee71fc63254a27f2450481a2f66288
Matt <matt@unique.net>
2021-11-15 09:22:20 -0800

    version update

--- Version.py
+++ Version.py
@@ -3,14 +3,15 @@ def GetScriptInfo():
     #Return Script Version Number (double) -> used to denote base master branch version these came from
     #Return Branch (string) -> "master branch" or specific customer
     #Return lastUpdateDate (string) -> Last day these scripts were updated
-    version = 1.4
+    version = 1.5
     branch = "Master"
-    lastUpdateDate = "9/15/2021"
+    lastUpdateDate = "11/15/2021"
     history = GetHistory()
     return version, branch, lastUpdateDate, history 
 
 def GetHistory():
     return (
+        "1.5 - 11/15/21 - 'Set_Sync' to as Avaya action handle to run set sync "
         "1.4 - 9/15/21 - Code restructure for constants.  Added Consts folder.\n"
             "\tAdded ReturnDictionary (instantiated in MyRequest as C# Dictionary<string,string>) to be used throughout.  It's added to the ScriptReturnData in the end.\n"
         "1.3 - 5/15/21 - fixed bug in getting version from Capitain.py\n"
